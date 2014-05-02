9df41a1d7d6405b6a0fb464aa90b80fec37f8e34
qwwdfsad <qwwdfsad@gmail.com>
2014-05-02 15:55:21 +0400

    DataSource bean added

--- codetrials-web/src/main/java/org/codetrials/server/config/AppConfig.java
+++ codetrials-web/src/main/java/org/codetrials/server/config/AppConfig.java
@@ -1,7 +1,11 @@
 package org.codetrials.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * @author Polyarnyi Nikolay
@@ -10,4 +14,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.codetrials.server")
 public class AppConfig {
 
+    @Bean
+    DataSource getDriverManagerDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/codeTrialsDB");
+        dataSource.setUsername("codeTrials");
+        dataSource.setPassword("12345");
+        return dataSource;
+    }
 }
