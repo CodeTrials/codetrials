d36fef83deb48f6baa3b21fb44c47d639a7e4bb7
Nikita <nikzyul@ya.ru>
2014-05-03 02:35:12 +0400

    CommonsMultipartResolver

--- codetrials-web/src/main/java/org/codetrials/server/config/WebMvcConfig.java
+++ codetrials-web/src/main/java/org/codetrials/server/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package org.codetrials.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -27,6 +28,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(100_000_000);
+        return resolver;
     }
 
     @Override
