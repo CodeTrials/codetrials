6912ec86a39d9e628d8bd3e78ab53f82a6d94619
qwwdfsad <qwwdfsad@gmail.com>
2014-05-02 13:01:27 +0400

    BundleDAO last commit fix

--- codetrials-web/src/main/java/org/codetrials/server/service/dao/MySqlBundleDAO.java
+++ codetrials-web/src/main/java/org/codetrials/server/service/dao/MySqlBundleDAO.java
@@ -1,21 +1,40 @@
 package org.codetrials.server.service.dao;
 
-import org.codetrials.server.service.entities.BundleDescription;
+import org.springframework.web.multipart.MultipartFile;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author qwwdfsad
  */
 public class MySqlBundleDAO implements BundleDAO {
+
+    private Connection connection = null;
+
+    public MySqlBundleDAO() throws Exception {
+        Properties cp = getConnectionProperties();
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/codeTrialsDB", cp);
+    }
+
+    private Properties getConnectionProperties() {
+        Properties connectionProp = new Properties();
+        connectionProp.put("characterEncoding","UTF8");
+        connectionProp.put("user", "codeTrials");
+        connectionProp.put("password", "12345");
+        return connectionProp;
+    }
+
     @Override
     public List<BundleDescription> getAllBundlesDescriptions() {
         throw new NotImplementedException();
     }
 
     @Override
-    public int addBundle(BundleDescription bd, String pathToJar) {
+    public int addBundle(String title, MultipartFile bundle) {
         throw new NotImplementedException();
     }
 }
