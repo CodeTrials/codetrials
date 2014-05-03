46d1e5345489ef328e15f17c8744b2631d90ab0c
Nikita <nikzyul@ya.ru>
2014-05-03 13:14:11 +0400

    bundle upload reaction

--- codetrials-web/src/main/java/org/codetrials/client/ContentManager.java
+++ codetrials-web/src/main/java/org/codetrials/client/ContentManager.java
@@ -10,6 +10,7 @@ import org.codetrials.client.core.events.Handler;
 import org.codetrials.client.trial.TrialPresenter;
 import org.codetrials.client.trialform.TrialFormPresenter;
 import org.codetrials.client.trialsgrid.TrialsGridPresenter;
+import org.codetrials.client.trialsmanager.TrialsManager;
 import org.codetrials.shared.LayoutConstants;
 import org.codetrials.shared.entities.Trial;
 
@@ -21,6 +22,9 @@ public class ContentManager {
     public static final EventType<Trial> SHOW_TRIAL = new EventType<>("SHOW_TRIAL");
     public static final EventType<Void> SHOW_INDEX = new EventType<>("SHOW_INDEX");
     public static final EventType<Void> SHOW_FORM = new EventType<>("SHOW_FORM");
+    private Widget trialsGridView;
+    private Widget trialView;
+    private Widget formView;
 
     @Inject
     public void initContainer(
@@ -30,40 +34,57 @@ public class ContentManager {
             final TrialFormPresenter formPresenter) {
         final RootPanel root = RootPanel.get(LayoutConstants.CONTAINER_ID);
 
-        final Widget trialsGridView = trialsGridPresenter.getView().asWidget();
-        final Widget trialView = trialPresenter.getView().asWidget();
-        final Widget formView = formPresenter.getView().asWidget();
+        trialsGridView = trialsGridPresenter.getView().asWidget();
+        trialView = trialPresenter.getView().asWidget();
+        formView = formPresenter.getView().asWidget();
 
         root.add(trialsGridView);
         root.add(trialView);
         root.add(formView);
 
-        trialView.setVisible(false);
-        formView.setVisible(false);
-
         bus.subscribe(SHOW_TRIAL, new Handler<Trial>() {
             @Override
             public void handle(Trial event) {
-                trialsGridView.setVisible(false);
-                formView.setVisible(false);
-                trialView.setVisible(true);
+                showTrial();
             }
         });
         bus.subscribe(SHOW_INDEX, new Handler<Void>() {
             @Override
             public void handle(Void event) {
-                trialView.setVisible(false);
-                formView.setVisible(false);
-                trialsGridView.setVisible(true);
+                showIndex();
             }
         });
         bus.subscribe(SHOW_FORM, new Handler<Void>() {
             @Override
             public void handle(Void event) {
-                trialView.setVisible(false);
-                trialsGridView.setVisible(false);
-                formView.setVisible(true);
+                showForm();
             }
         });
+        bus.subscribe(TrialsManager.NEW_TRIAL, new Handler<Trial>() {
+            @Override
+            public void handle(Trial event) {
+                showIndex();
+            }
+        });
+
+        showIndex();
+    }
+
+    private void showForm() {
+        trialView.setVisible(false);
+        trialsGridView.setVisible(false);
+        formView.setVisible(true);
+    }
+
+    private void showIndex() {
+        trialView.setVisible(false);
+        formView.setVisible(false);
+        trialsGridView.setVisible(true);
+    }
+
+    private void showTrial() {
+        trialsGridView.setVisible(false);
+        formView.setVisible(false);
+        trialView.setVisible(true);
     }
 }
--- codetrials-web/src/main/java/org/codetrials/client/trialform/TrialFormPresenterImpl.java
+++ codetrials-web/src/main/java/org/codetrials/client/trialform/TrialFormPresenterImpl.java
@@ -1,13 +1,24 @@
 package org.codetrials.client.trialform;
 
+import com.google.gwt.json.client.JSONBoolean;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
+import org.codetrials.client.core.events.EventBus;
 import org.codetrials.client.core.logging.Log;
 import org.codetrials.client.core.mvp.BasePresenter;
+import org.codetrials.client.trialsmanager.JsTrial;
+import org.codetrials.client.trialsmanager.TrialsManager;
 
 /**
  * @author Nikita Zyulyaev
  */
 class TrialFormPresenterImpl extends BasePresenter<TrialFormView> implements TrialFormPresenter {
+    @Inject
+    EventBus bus;
+
     @Inject
     TrialFormPresenterImpl(TrialFormView view) {
         super(view);
@@ -15,6 +26,15 @@ class TrialFormPresenterImpl extends BasePresenter<TrialFormView> implements Tri
 
     @Override
     public void onBundleUploaded(String results) {
-        Log.info(results);
+        JSONObject result = (JSONObject) JSONParser.parseStrict(results);
+        boolean success = ((JSONBoolean) result.get("success")).booleanValue();
+        if (success) {
+            JSONObject jsTrial = ((JSONObject) result.get("newTrial"));
+            bus.fire(TrialsManager.NEW_TRIAL, jsTrial.getJavaScriptObject().<JsTrial>cast().toTrial());
+        } else {
+            JSONString message = (JSONString) result.get("message");
+            Window.alert(message.stringValue());
+            Log.warn(message.stringValue());
+        }
     }
 }
